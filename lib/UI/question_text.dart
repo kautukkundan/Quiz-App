45e446cfc91e016565fd46dca969a2fd0224b235
kautukkundan <kautukkundan@gmail.com>
2018-06-22 15:29:30 +0530

    added animation to question

--- lib/UI/question_text.dart
+++ lib/UI/question_text.dart
@@ -10,14 +10,30 @@ class QuestionText extends StatefulWidget {
   State createState() => new QuestionTextState();
 }
 
-class QuestionTextState extends State<QuestionText> {
+class QuestionTextState extends State<QuestionText> with SingleTickerProviderStateMixin {
+
+  Animation<double> _fontSizeAnimation;
+  AnimationController _fontSizeAnimationController;
+
+  @override
+  void initState(){
+    super.initState();
+    _fontSizeAnimationController = new AnimationController(duration: new Duration(milliseconds: 500), vsync: this);
+    _fontSizeAnimation = new CurvedAnimation(parent: _fontSizeAnimationController, curve: Curves.bounceOut);
+    _fontSizeAnimation.addListener(()=> this.setState(() {}));
+    _fontSizeAnimationController.forward();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return new Material(
       color: Colors.white,
       child: new Padding(
         padding: new EdgeInsets.symmetric(vertical: 20.0),
         child: new Center(
-        child: new Text("Statement " + widget._questionNumber.toString() +": " + widget._question),
+        child: new Text("Statement " + widget._questionNumber.toString() +": " + widget._question,
+        style: new TextStyle(fontSize: _fontSizeAnimation.value * 15),
+        ),
       ),
       ),
     );
